9e9fbe37ba9197baa61ddc0c28bb5692d320da3a
jeongjuryu <totw2018@naver.com>
2021-01-29 10:11:23 +0900

    react-hooks

---
+++ useEffect/useEffect.js
@@ -0,0 +1,15 @@
+function App() {
+  const sayHello = () => console.log("say Hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(() => {
+    sayHello();
+  }, [number, aNumber]);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+}
