e643812671584600fa9b617a43fed1b900401b74
jeongjuryu <totw2018@naver.com>
2021-01-31 10:11:23 +0900

    react-hooks

--- useInput/useTabs.js
+++ useInput/useTabs.js
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       {content.map((section, index) => (
-        <button onClick={changeItem(index)}>{section.tab}</button>
+        <button onClick={() => changeItem(index)}>{section.tab}</button>
       ))}
       <div>{currentItem.content}</div>
     </div>
