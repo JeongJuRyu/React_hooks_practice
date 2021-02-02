21742f0e7ffa2d84351855d2446f396fa00a6447
jeongjuryu <totw2018@naver.com>
2021-02-02 10:11:23 +0900

    react-hooks

---
+++ useEffect/useClick.js
@@ -0,0 +1,25 @@
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+function App() {
+  const sayHello = () => {
+    console.log("say hi");
+  };
+  const title = useClick(sayHello);
+  return (
+    <div className="App">
+      <h1 ref={title}>hi</h1>
+    </div>
+  );
+}
