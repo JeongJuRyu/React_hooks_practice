1a3904396003bc40eb4f5bf056275c4693c30d45
jeongjuryu <totw2018@naver.com>
2021-02-08 14:23:45 +0900

    react-hooks

--- useEffect/useScroll.js
+++ useEffect/useScroll.js
@@ -1,23 +1,27 @@
+
 const useScroll = () => {
-    const [state, setState] = useState({
-        x:0,
-        y:0,
-    });
-    const onScroll = (e) => {
-        console.log(e);
-    }
-    useEffect(()=> {
-        window.addEventListener('scroll', onScroll);
-        return () =>  window.removeEventListener('scroll', onscroll);
-    }, [])
-    return state;
-}
+  const [state, setState] = useState({
+    x: 0,
+    y: 0
+  });
+  const onScroll = () => {
+    setState({ y: window.scrollY, x: window.scrollX });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onscroll);
+  }, []);
+  return state;
+};
 
 const App = () => {
-    const {y} = useScroll();
-    return (
-        <div className="App" style = {{height:"1000vh"}}>
-            <h1 style={{position : 'fixed', color : y > 100 ? "blue" : "red"}}>hello</h1>
-        </div>
-    )
-}
+  const { y } = useScroll();
+  console.log(y);
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <h1 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+        hello
+      </h1>
+    </div>
+  );
+};
